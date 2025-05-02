1b2f66d3f6888182a8e3d2a99e6aefb1b163abb1
A-afflatus <wucuo456@gmail.com>
2025-05-02 12:06:19 +0800

    添加通知组件

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -20,6 +20,7 @@ pub fn run() {
     credential::get_s3_pool();
     //启动ui
     tauri::Builder::default()
+        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
